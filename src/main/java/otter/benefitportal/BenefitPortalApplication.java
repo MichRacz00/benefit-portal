b7e2282071b6c9fd9be1db31c5dba225c35f6bdf
MichRacz00 <michal.raczkiewicz@hotmail.com>
2026-02-10 08:46:27 +0100

    initial commit

---
+++ src/main/java/otter/benefitportal/BenefitPortalApplication.java
@@ -0,0 +1,13 @@
+package otter.benefitportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/otter/benefitportal/BenefitPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package otter.benefitportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
